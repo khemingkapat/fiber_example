dd7cdda6684294d83c90da1c55bd370ef178715c
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-03-03 21:26:37 +0700

    importing package

--- main.go
+++ main.go
@@ -1,34 +1,27 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"log"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/khemingkapat/fiber_example/handlers"
+	"github.com/khemingkapat/fiber_example/queries"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	// Connection string (hardcoded credentials)
-	connStr := "user=admin password=adminpassword dbname=dorm host=localhost port=5430 sslmode=disable"
-
-	// Open a connection to the database
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	// Ping the database to verify the connection
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	fmt.Println("Successfully connected to the database!")
+	db, err := queries.InitDB()
+	if err != nil {
+		log.Println(err)
+	}
+	app := fiber.New()
 
-	// You can now perform database operations using the 'db' object
-	// Example:
-	// rows, err := db.Query("SELECT * FROM your_table")
-	// ...
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("fiber example for dorm management")
+	})
+	app.Get("/people", handlers.GetPeopleHandler(db))
+
+	log.Fatal(app.Listen(":8080"))
 }
