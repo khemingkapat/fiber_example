ae1e927395d22b1dc82746b29fc987b9c9399a3b
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-03-08 22:44:07 +0700

    organize

---
+++ handlers/queries/deletePersonHandler.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"strconv"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/khemingkapat/fiber_example/queries"
+	"gorm.io/gorm"
+)
+
+func DeletePersonHandler(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		id, err := strconv.Atoi(c.Params("id"))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString("Error Getting ID")
+		}
+		err = queries.DeletePerson(db, uint(id))
+		if err != nil {
+			return c.Status(fiber.StatusBadRequest).SendString(err.Error())
+		}
+		return c.JSON(fiber.Map{"message": "Person Deleted"})
+	}
+}
