5f1e42fada1072f700983addc416d034cb210b74
khemingkapat <60930364+khemingkapat@users.noreply.github.com>
2025-03-08 15:47:24 +0700

    rename file for util functions

--- utils.go
+++ utils.go
@@ -33,3 +33,21 @@ func populateDB(db *gorm.DB) {
 	}
 	fmt.Println("DB Populated")
 }
+
+func connStrSelect(mode string) string {
+	var host string
+	var port int
+
+	if mode == "postgres" {
+		host = "postgres"
+		port = 5432
+	} else if mode == "localhost" {
+		host = "localhost"
+		port = 5430
+	} else {
+		panic("Invalid Mode")
+	}
+
+	result := fmt.Sprintf("user=admin password=admin dbname=dorm host=%s port=%d sslmode=disable", host, port)
+	return result
+}
